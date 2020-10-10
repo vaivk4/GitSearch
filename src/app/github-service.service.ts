1c8320ece4c42331617fbb347274deaa4d304f8b
vaivk4 <vai.vk4@gmail.com>
2020-10-10 22:51:52 +0530

    repositoy collection added

--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { RepoArray } from './user';
+
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -11,25 +13,13 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root'
 })
-// export class GithubServiceService {
 
-//   constructor() { }
-// }
 
 export class GithubServiceService {
 
   constructor(private http: HttpClient) { }
 
   configUrl = 'https://api.github.com';
-
-
-  // getRepos(userName: string): Observable<RepoArray[]> {
-  //   const url = `${this.configUrl}/users/${userName}/repos`
-  //   return this.http.get<RepoArray[]>(url)
-  //   .pipe(
-  //       catchError(this.handleError('fetch users', []))
-  //     );
-  // }
 
   getOrgaRepos(userName: string): Observable<RepoArray[]> {
     const url = `${this.configUrl}/orgs/${userName}/repos`;
