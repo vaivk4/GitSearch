d7137901f172197b40bc403fe35ee629c23906fb
vaivk4 <vai.vk4@gmail.com>
2020-10-10 22:43:36 +0530

    github services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class RepoArray {
+  id: number;
+  name: string;
+  size: number;
+  language: any;
+  description: string;
+  stargazers_count: number;
+  watchers_count: number;
+  forks_count: number;
+  fork: boolean;
+  html_url: string;
+}
