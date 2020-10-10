cdfe097fe6d4e190f4265405861a484e2ef5f1cd
vaivk4 <vai.vk4@gmail.com>
2020-10-11 00:02:31 +0530

    app module and style modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GithubServiceService } from './github-service.service'
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { RepositoryComponent } from './repository/repository/repository.component';
+import { HttpClientModule } from '@angular/common/http'
+import { TruncatePipe } from '../truncate.pipe';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { RepositoryComponent } from './repository/repository/repository.componen
     RepositoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
