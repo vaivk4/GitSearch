0dd27679632ee46cb7de61bb3e64e037a7c8c6fb
vaivk4 <vai.vk4@gmail.com>
2020-10-11 09:41:20 +0530

    deploying on heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
+
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
+app.use(express.static(__dirname + '/dist/git-search'));
+
 app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+
+res.sendFile(path.join(__dirname+'/dist/git-search'));
 });
- 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
